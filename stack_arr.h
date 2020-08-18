691071d4b4040443880277084d3c1912316c801a
Ennis M Salam <47441850+leBigChungus@users.noreply.github.com>
2020-08-18 17:33:49 +0400

    Completed Project
    Created:
    1. main
    2. Lots of comments
    
    Fixed bugs in:
    1. main
    2. isFull
    3. Pop

--- stack_arr.h
+++ stack_arr.h
@@ -1,6 +1,3 @@
-//stack_arr.h
-//Add documentation including Pre and Post conditions for functions
-
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -13,12 +10,37 @@ typedef struct Stack312 {
     StackEntry elements[MAX_ELEMENTS];
 } Stack312;
 
+
+//Function: Initialize the stack
+//Psuedo: Initialize the stack (array format) to 'z' and pointer to -1(empty)
+//Inputs: 1. Pointer to stack
+//Outputs: N/A
 void makeStack(Stack312* s);
 
+
+//Function: Check to see if stack is full
+//Psuedo: If top value is initialized value, stack is not full
+//Inputs: 1. stack itself
+//Outputs: 1 if Full, 0 if not Full
 bool isFull(Stack312 s);
 
+
+//Function: Check to see if stack is empty
+//Psuedo: Check if counter of top of stack is less than zero
+//Inputs: 1. stack itself
+//Outputs: 1 if Empty, 0 if not Empty
 bool isEmpty(Stack312 s);
 
+
+//Function: Push onto stack. Do not push if full
+//Psuedo: If the stack is full, do not push. Assumption that no equation will have more than 100 brackets/parenthesis
+//Inputs: 1. value to be pushed 2.pointer to stack
+//Outputs: N/A
 void push(StackEntry e,Stack312* s);
 
+
+//Function: Pop from stack
+//Psuedo: If the stack is empty, do not pop
+//Inputs: 1. pointer to stack
+//Outputs: 1. value popped from stack
 StackEntry pop(Stack312* s);
--- stack_arr_driver.c
+++ stack_arr_driver.c
@@ -1,3 +1,60 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include "stdlib.h"
+#include "stack_arr.h"
 
+int main(void) {
+    Stack312 *txtFileStack;
+    StackEntry poppedChar;
+    FILE *ptr_file;
+    char charFromFile;
+    int invalidExpressionCounter = 0;
+
+    //create stack
+    txtFileStack = (Stack312 *) malloc(sizeof(Stack312));     //typecasting not necessarily needed. kept for C++ compatibility
+    makeStack(txtFileStack);
+
+    //Open file and check if empty
+    ptr_file = fopen("../fake_picture.txt", "r");     // "./" is important!!!!
+    if (ptr_file == NULL) {
+        printf("error. file is null");
+        return 0;
+    }
+
+    //Routine to print txt file, then reset file pointer
+    while ((charFromFile = fgetc(ptr_file)) != EOF) {
+        printf("%c", charFromFile);
+    }
+    printf("\n");
+    ptr_file = fopen("../fake_picture.txt", "r");     // "./" is important!!!!
+
+    //Routine to check symmetry
+    while ((charFromFile = fgetc(ptr_file)) != EOF) {     //First line received and placed in txtFileString
+
+        if (charFromFile == '[' || charFromFile == '(') {
+            push(charFromFile, txtFileStack);
+        } else if (charFromFile == ']') {
+            poppedChar = pop(txtFileStack);
+            if (poppedChar != '[') {
+                printf("Missing  ) \n");
+                invalidExpressionCounter++;
+            }
+        } else if (charFromFile == ')') {
+            poppedChar = pop(txtFileStack);
+            if (poppedChar != '(') {
+                printf("Missing  ] \n");
+                invalidExpressionCounter++;
+            }
+        }
+
+        //if it is symmetrical, move on
+
+    }
+
+    if (invalidExpressionCounter > 0){
+        printf("Invalid expression");
+    }
+    else{
+        printf("Valid expression");
+    }
+    return 0;
+}
